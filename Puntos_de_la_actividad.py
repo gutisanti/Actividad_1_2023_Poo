18869e389718f1d22c7660195366499005dcfa91
Santiago <santiago.guti17correa@gmail.com>
2023-08-02 19:50:26 -0500

    Commit 5

--- Puntos_de_la_actividad.py
+++ Puntos_de_la_actividad.py
@@ -1,12 +1,12 @@
 #punto 1
 def main():
-    # Solicitar el nombre al usuario
+    
     nombre = input("Por favor, introduce tu nombre: ")
 
-    # Solicitar la edad al usuario
+    
     edad = input("Por favor, introduce tu edad: ")
 
-    # Mostrar el nombre y la edad en pantalla
+    
     print("Hola, " + nombre + "! Tienes " + edad + " años.")
 
 if __name__ == "__main__":
