aaa03390c27b2000620b7c35d507823f993a1806
Santiago <santiago.guti17correa@gmail.com>
2023-08-02 19:25:14 -0500

    First_commit

---
+++ Puntos_de_la_actividad.py
@@ -0,0 +1,16 @@
+#punto 1
+def main():
+    # Solicitar el nombre al usuario
+    nombre = input("Por favor, introduce tu nombre: ")
+
+    # Solicitar la edad al usuario
+    edad = input("Por favor, introduce tu edad: ")
+
+    # Mostrar el nombre y la edad en pantalla
+    print("Hola, " + nombre + "! Tienes " + edad + " años.")
+
+if __name__ == "__main__":
+    main()
+
+
+    
