307f2257ea98ca2427c5aaa586d893712de5ee49
Santiago <santiago.guti17correa@gmail.com>
2023-08-02 19:33:16 -0500

    Commit 2

--- Puntos_de_la_actividad.py
+++ Puntos_de_la_actividad.py
@@ -12,5 +12,17 @@ def main():
 if __name__ == "__main__":
     main()
 
+#punto 2
+import math
+
+def calcular_area_circulo(radio):
+    area = math.pi * radio**2
+    return area
+
+radio = 5
+area = calcular_area_circulo(radio)
+print("El área del círculo es:", area)
+
+
 
     
