ba90d8bc022d2306ff8a7a61e46853a0da3c3d62
Santiago <santiago.guti17correa@gmail.com>
2023-08-02 19:38:14 -0500

    Commit 3

--- Puntos_de_la_actividad.py
+++ Puntos_de_la_actividad.py
@@ -24,5 +24,46 @@ area = calcular_area_circulo(radio)
 print("El área del círculo es:", area)
 
 
+#punto 3
+import random
+
+def generar_lista_numeros_aleatorios(n):
+    lista_aleatoria = [random.randint(1, 100) for _ in range(n)]
+    return lista_aleatoria
+
+cantidad_numeros = int(input("Ingresa la cantidad de números aleatorios que deseas generar: "))
+numeros_aleatorios = generar_lista_numeros_aleatorios(cantidad_numeros)
+print("Lista de números aleatorios:", numeros_aleatorios)
+
+
+#punto 4
+def es_par(numero):
+    return numero % 2 == 0
+
+numero = int(input("Ingresa un número entero: "))
+if es_par(numero):
+    print(numero, "es un número par.")
+else:
+    print(numero, "es un número impar.")
+
+
+#punto 5
+def fahrenheit_a_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+fahrenheit = float(input("Ingresa la temperatura en grados Fahrenheit: "))
+celsius = fahrenheit_a_celsius(fahrenheit)
+print("La temperatura en grados Celsius es:", celsius)
+
+
+#punto 6
+def calcular_suma_lista(lista):
+    suma = sum(lista)
+    return suma
+
+lista_numeros = [1, 2, 3, 4, 5]
+suma = calcular_suma_lista(lista_numeros)
+print("La suma de los números en la lista es:", suma)
 
     
